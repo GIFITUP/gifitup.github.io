ad454c159c323103bc91002a1a433e901f823243
GIFITUP <43257995+GIFITUP@users.noreply.github.com>
2018-09-14 17:45:32 -0400

    Update main.js

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ $(document).ready(function() {
    // httpGetAsync('http://api.giphy.com/v1/gifs/random?'+ params, function(data) {
  httpGetAsync('http://api.giphy.com/v1/gifs/random?EOvxPf7jMMtXmpW6GVGndluHTFeJdFd6', function(data) {
 
-    var gifs = JSON.parse(data);
+      var gifs = JSON.parse(data);
       var firstgif = gifs.data[0].images.original.url;
       $("#image").html("<img src='" + firstgif + "'>");
       console.log(gifs.data);
