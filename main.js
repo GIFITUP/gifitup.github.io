bd3ef5a7b57cdd0c1bddfe8a4a4f7136b0f06067
GIFITUP <43257995+GIFITUP@users.noreply.github.com>
2018-09-14 19:17:22 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //var apikey = 'EOvxPf7jMMtXmpW6GVGndluHTFeJdFd6';
 var apikey = 'dc6zaTOxFJmzC';
-
+$("#image").html("<img src='"http://media2.giphy.com/media/FiGiRei2ICzzG/200.gif"'>");
 $(document).ready(function() {
   
   /* 
