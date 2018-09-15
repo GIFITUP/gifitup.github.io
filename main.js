25fb239a56f3f3f00c588481a91557b7bff938b7
GIFITUP <43257995+GIFITUP@users.noreply.github.com>
2018-09-15 12:46:45 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-//var apikey = 'EOvxPf7jMMtXmpW6GVGndluHTFeJdFd6';
-var apikey = 'dc6zaTOxFJmzC';
-//$("#image").html("<img src='"http://media2.giphy.com/media/FiGiRei2ICzzG/200.gif"'>");
+var apikey = 'EOvxPf7jMMtXmpW6GVGndluHTFeJdFd6';
+
 $(document).ready(function() {
   
   /* 
@@ -40,11 +39,9 @@ $(document).ready(function() {
 
     // api from https://github.com/Giphy/GiphyAPI#search-endpoint 
 
-     httpGetAsync('http://api.giphy.com/v1/gifs/random?' + params, function(data) {
- //     httpGetAsync('http://api.giphy.com/v1/gifs/random?', function(data) {
-//http://api.giphy.com/v1/gifs/random?api_key=EOvxPf7jMMtXmpW6GVGndluHTFeJdFd6
+    httpGetAsync('http://api.giphy.com/v1/gifs/search?' + params, function(data) {
       var gifs = JSON.parse(data);
-      var firstgif = gifs.data[0].images.original.url;
+      var firstgif = gifs.data[0].images.fixed_width.url;
       $("#image").html("<img src='" + firstgif + "'>");
       console.log(gifs.data);
     });
