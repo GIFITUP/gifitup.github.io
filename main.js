ed34d8220afec4a2e43bfde7b9ba67daf841608e
GIFITUP <43257995+GIFITUP@users.noreply.github.com>
2018-09-15 13:15:14 -0400

    Changed to RANDOM

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ $(document).ready(function() {
 
     // api from https://github.com/Giphy/GiphyAPI#search-endpoint 
 
-    httpGetAsync('http://api.giphy.com/v1/gifs/search?' + params, function(data) {
+    httpGetAsync('http://api.giphy.com/v1/gifs/random?' + params, function(data) {
       var gifs = JSON.parse(data);
       var firstgif = gifs.data[0].images.original.url;
       $("#image").html("<img src='" + firstgif + "'>");
