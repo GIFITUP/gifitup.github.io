b45f0b7d75e986f9db7e7982b5e9215cd8f35cf1
GIFITUP <43257995+GIFITUP@users.noreply.github.com>
2018-09-15 19:35:59 -0400

    Update giphy.js

--- giphy.js
+++ giphy.js
@@ -1,19 +1,9 @@
-// on page load, search for & display a random gif matching your search term using the Giphy API.
-// usage: 
-//   include giphy.js in your <head>
-//   set q to your search term (e.g. "brunch")
-//   add <span id = "giphyme"></span> wherever you want to display the image. -- FYI, it will be centered.
-// big ups to the Giphy crew (giphy.com)
-// 2014 - Neal Shyam [@nealrs | nealshyam.com]
-//$("#submitButton").on("click", function() {
-
-
 document.addEventListener('DOMContentLoaded', function () {
 	q = "finger guns"; // search query
+	
 	request = new XMLHttpRequest;
 	request.open('GET', 'http://api.giphy.com/v1/gifs/random?api_key=EOvxPf7jMMtXmpW6GVGndluHTFeJdFd6', true);
 	
-//	$("#submitButton").on("click", function () {
 	request.onload = function() {
 		if (request.status >= 200 && request.status < 400){
 			data = JSON.parse(request.responseText).data.image_url;
@@ -30,4 +20,3 @@ document.addEventListener('DOMContentLoaded', function () {
 
 	request.send();
 });
-//};
