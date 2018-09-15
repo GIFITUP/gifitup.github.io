27c50a5d534991dec5b8b36a251944a01007a2c9
GIFITUP <43257995+GIFITUP@users.noreply.github.com>
2018-09-15 19:10:44 -0400

    Update giphy.js

--- giphy.js
+++ giphy.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function () {
 	q = "finger guns"; // search query
 	
 	request = new XMLHttpRequest;
-	request.open('GET', 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag='+q, true);
+	request.open('GET', 'http://api.giphy.com/v1/gifs/random?api_key=EOvxPf7jMMtXmpW6GVGndluHTFeJdFd6', true);
 	
 	request.onload = function() {
 		if (request.status >= 200 && request.status < 400){
